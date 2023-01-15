4f0c1e5f7e87ac13549411420a95f3badb365787
kasun@xgengroup.com.au <kasunvimu8@gmail.com>
2023-01-16 02:41:14 +0530

    queue from stack implementation

--- main.py
+++ main.py
@@ -1,65 +1,26 @@
 from data_structures.stack import Stack
 
-class InfixToPostFix:
-    operations = ['+', '-', '/', '*', '(', ')']
+class QueueFromStacks:
 
     def __init__(self, str):
-      self.result = ''
-      self.input = str
-
-    def is_operator(self, x):
-      return x in self.operations
-
-    def check_precedence(self, operator):
-      switcher = {
-        '+' : 1,
-        '-' : 1,
-        '*' : 2,
-        '/' : 2,
-      }
-      return switcher.get(operator, -1)
-
-    def is_closing_operator(self, operator):
-      return operator == ')'
-    def is_opening_operator(self, operator):
-      return operator == '('
-  
-    def convert(self):
-      s = Stack()
-      out = ''
-
-      for c in self.input:
-        if self.is_operator(c):
-          if self.is_closing_operator(c):
-            i = 0
-            while i < s.size():
-              item = s.pop()
-              if self.is_opening_operator(item):
-                break
-              out+=str(item)
-          else:
-            top_operator = s.peek()
-            if top_operator is None:
-              s.push(c)
-            else:
-              if self.check_precedence(top_operator) < self.check_precedence(c):
-                s.push(c)
-              else:
-                out+=str(s.pop())
-                s.push(c)
-        else:
-          out+=str(c)
+      self.push_stack = Stack()
+      self.pop_stack = Stack()
       
-      j = 0
-      while j < s.size():
-        item = s.pop()
-        out+=str(item)
-      
-      return out
+    def is_empty(self):
+      return self.push_stack == [] and self.pop_stack == [];
+
+    # def enqueue(self, item) :
+    #   self.items.insert(0, item)
+    
+    # def dequeue(self):
+    #   return self.items.pop();
+
+    def size(self):
+      return len(self.push_stack) + len(self.pop_stack)
+    
         
-convert_to_postfix = InfixToPostFix('(3+4)*2+1')
-print(convert_to_postfix.convert())
-
-# class Stack(list):
-#     push = list.append
-#     peek = lambda self: self[-1]
+queue = QueueFromStacks();
+# queue.is_empty()
+# queue.enqueue(2)
+# queue.dequeue()
+# queue.size()
