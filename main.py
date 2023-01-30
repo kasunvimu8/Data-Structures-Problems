4e0af0192c8afc78a6a1e5906f9c72c0ccd83e80
kasun <kasunvimu8@gmail.com>
2023-01-31 00:44:45 +0530

    Balance Paranthesis Problem solved

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 from stacks_and_queues.stack import Stack
 
-# check the paranthesis matching
+# check the paranthesis matching, important for compilers
 
+def is_balance_paranthesis(arg1, arg2):
+  if ((arg1 == '(' and arg2 == ')') or
+      (arg1 == '{' and arg2 == '}') or
+      (arg1 == '[' and arg2 == ']') or
+      (arg1 == '<' and arg2 == '>')):
+    return True
+  else:
+    return False
+  
 
-def isOpenningParanthesis(arg):
-    switcher = {
-        '(': True,
-        '{': True,
-        '[': True,
-        '<': True,
-    }
-    return switcher.get(arg, False)
+def is_openning_paranthesis(arg):
+  switcher = {
+      '(': True,
+      '{': True,
+      '[': True,
+      '<': True,
+  }
+  return switcher.get(arg, False)
 
 
 def balance_paranthesis_problem(paranthesis):
-    stack = Stack()
-    for para in paranthesis:
-        print(para)
+  stack = Stack()
+  status = True
 
-    return paranthesis[4]
+  for para in paranthesis:
+    if is_openning_paranthesis(para):
+      stack.push(para)
+    else:
+      if stack.is_empty():
+        status = False
+        break
+      else:
+        if is_balance_paranthesis(stack.pop(), para):
+          continue
+        else:
+          status = False
+          break
+  return status
 
 
-balanced = balance_paranthesis_problem("(({}))")
+balanced = balance_paranthesis_problem("(({?}[][]))")
 print(balanced)
---
+++ stacks_and_queues/balance_paranthesis_probelm.py
@@ -0,0 +1,40 @@
+from stacks_and_queues.stack import Stack
+
+def is_balance_paranthesis(arg1, arg2):
+  if ((arg1 == '(' and arg2 == ')') or
+      (arg1 == '{' and arg2 == '}') or
+      (arg1 == '[' and arg2 == ']') or
+      (arg1 == '<' and arg2 == '>')):
+    return True
+  else:
+    return False
+  
+
+def is_openning_paranthesis(arg):
+  switcher = {
+      '(': True,
+      '{': True,
+      '[': True,
+      '<': True,
+  }
+  return switcher.get(arg, False)
+
+
+def balance_paranthesis_problem(paranthesis):
+  stack = Stack()
+  status = True
+
+  for para in paranthesis:
+    if is_openning_paranthesis(para):
+      stack.push(para)
+    else:
+      if stack.is_empty():
+        status = False
+        break
+      else:
+        if is_balance_paranthesis(stack.pop(), para):
+          continue
+        else:
+          status = False
+          break
+  return status
