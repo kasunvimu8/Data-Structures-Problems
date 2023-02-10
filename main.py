13cd08d6e9c7aa177c1995f7413f1689a2d60daf
kasun <kasunvimu8@gmail.com>
2023-02-10 09:30:49 +0530

    sorting algo quick sort started

--- main.py
+++ main.py
@@ -1,46 +1,18 @@
-from stacks_and_queues.stack import Stack
-
-# check the paranthesis matching, important for compilers
-
-def is_balance_paranthesis(arg1, arg2):
-  if ((arg1 == '(' and arg2 == ')') or
-      (arg1 == '{' and arg2 == '}') or
-      (arg1 == '[' and arg2 == ']') or
-      (arg1 == '<' and arg2 == '>')):
-    return True
-  else:
-    return False
-  
-
-def is_openning_paranthesis(arg):
-  switcher = {
-      '(': True,
-      '{': True,
-      '[': True,
-      '<': True,
-  }
-  return switcher.get(arg, False)
+def swap(arr, index1, index2):
+  temp = arr[index1]
+  arr[index1] = arr[index2]
+  arr[index2] = temp
+  return arr
 
 
-def balance_paranthesis_problem(paranthesis):
-  stack = Stack()
-  status = True
-
-  for para in paranthesis:
-    if is_openning_paranthesis(para):
-      stack.push(para)
-    else:
-      if stack.is_empty():
-        status = False
-        break
-      else:
-        if is_balance_paranthesis(stack.pop(), para):
-          continue
-        else:
-          status = False
-          break
-  return status
+def quick_sort(array):
+  pivot = array[len(array) -1]
+  i=0
+  while i < (len(array) -1):
+    if(array[i] > pivot):
+      swap(array, i , )
+  return array
 
 
-balanced = balance_paranthesis_problem("(({?}[][]))")
-print(balanced)
+sorted_array = quick_sort([2, 7, 4, 3, 6, 1, 5, 4])
+print(sorted_array)
