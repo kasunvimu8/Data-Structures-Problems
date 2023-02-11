c39ae56083e7b6ab72a1c06fb11009eccf17937f
kasun <kasunvimu8@gmail.com>
2023-02-12 01:42:57 +0530

    Quick Sort Implementation

--- main.py
+++ main.py
@@ -5,14 +5,35 @@ def swap(arr, index1, index2):
   return arr
 
 
-def quick_sort(array):
-  pivot = array[len(array) -1]
-  i=0
-  while i < (len(array) -1):
+def quick_sort(array, start, end):
+  pivot = array[end]
+  i=start
+  j=end -1
+  
+  while i < (end -1):
+    
     if(array[i] > pivot):
-      swap(array, i , )
+      
+      while j != i:
+        if (i == j):
+          break
+        
+        if(array[j] <= pivot):
+          swap(array, i, j)
+          j=j-1
+          break
+        
+        j=j-1
+      
+    if(i == j):
+      swap(array, i+1, len(array) -1)
+      quick_sort(array, start, i)
+      quick_sort(array, i+2, end)
+      break
+        
+    i=i+1
+
   return array
-
-
+ 
 sorted_array = quick_sort([2, 7, 4, 3, 6, 1, 5, 4])
 print(sorted_array)
