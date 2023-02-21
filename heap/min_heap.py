b4d6a21c8cf3bfefccab6b9a360d5e74016c615c
kasun <kasunvimu8@gmail.com>
2023-02-21 12:12:55 +0530

    Min Heap implementation done

---
+++ heap/min_heap.py
@@ -0,0 +1,83 @@
+import math
+
+# Tree Based Data Structure
+# complete binary tree
+# parent should be less or equal than it's children (Min Heap)
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    # swap the two elements
+    def __swap(self, first_index, second_index):
+        self.heap[first_index], self.heap[second_index] = self.heap[second_index], self.heap[first_index]
+
+    # return the height of the heap
+    def heap_height(self):
+        return math.ceil(math.log2(len(self.heap)))
+
+    # return parent index
+    def parent_index(self, child_index):
+        return child_index // 2
+
+    # return left child index
+    def left_child_index(self, p):
+        return 2*p + 1
+
+    # return right child index
+    def right_child_index(self, p):
+        return 2*p + 2
+
+    def is_leaf(self, i):
+        return self.left_child_index(i) + 1 > len(self.heap)
+
+    def heapify(self, i):
+        left_child_index = self.left_child_index(i)
+        right_child_index = self.right_child_index(i)
+        parent = self.heap[i]
+
+        if not self.is_leaf(i):
+            if parent > self.heap[left_child_index] or (right_child_index + 1 < len(self.heap) and parent > self.heap[right_child_index]):
+                if self.heap[left_child_index] > self.heap[left_child_index]:
+                    self.__swap(i, left_child_index)
+                    self.heapify(left_child_index)
+                else:
+                    self.__swap(i, right_child_index)
+                    self.heapify(right_child_index)
+
+    def min_heapify(self):
+        h = self.heap_height()
+
+        for i in range(h-1, 0, -1):
+            self.heapify(i)
+
+    def insert(self, item):
+        self.heap.append(item)
+        current = len(self.heap) - 1
+
+        while self.heap[current] < self.heap[self.parent_index(current)]:
+            self.__swap(current, self.parent_index(current))
+            current = self.parent_index(current)
+
+    def remove(self):
+        self.__swap(0, len(self.heap)-1)
+        min = self.heap.pop()
+        self.min_heapify()
+        return min
+
+    def peek(self):
+        return self.heap[0]
+
+    def size(self):
+        return len(self.heap)
+
+    # Print the Heap Left and Right Elements
+    def print(self):
+        for i in range(0, (len(self.heap) // 2)):
+            right_child = 'NO CHILD'
+            if self.right_child_index(i) < len(self.heap):
+                right_child = str(self.heap[2*i + 2])
+
+            print("PARENT :" + str(self.heap[i]) + "   LEFT CHILD   ",
+                  str(self.heap[2*i + 1]), "  RIGHT CHILD " + right_child)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 
 # Tree Based Data Structure
 # complete binary tree
@@ -7,41 +8,77 @@ class MinHeap:
     def __init__(self):
       self.heap = []
     
-    # return parent index
-    def __parent(self, child_index):
-      return child_index // 2
-    
+    # swap the two elements
     def __swap(self, first_index, second_index):
       self.heap[first_index], self.heap[second_index] = self.heap[second_index], self.heap[first_index]
+    
+    # return the height of the heap
+    def heap_height(self):
+      return math.ceil(math.log2(len(self.heap)))
+    
+    # return parent index
+    def parent_index(self, child_index):
+      return child_index // 2
+    
+    # return left child index
+    def left_child_index(self, p):
+      return 2*p +1
+    
+    # return right child index
+    def right_child_index(self, p):
+      return 2*p +2
+    
+    def is_leaf(self, i):
+      return self.left_child_index(i) +1 > len(self.heap)
       
+    def heapify(self, i):
+      left_child_index = self.left_child_index(i)
+      right_child_index =  self.right_child_index(i)
+      parent = self.heap[i]
+      
+      if not self.is_leaf(i):
+        if parent > self.heap[left_child_index] or (right_child_index +1 < len(self.heap) and parent > self.heap[right_child_index]):
+          if self.heap[left_child_index] > self.heap[left_child_index]:
+            self.__swap(i, left_child_index)
+            self.heapify(left_child_index)
+          else:
+            self.__swap(i, right_child_index)
+            self.heapify(right_child_index)
+    
     def min_heapify(self):
-      current = len(self.heap) -1
+      h = self.heap_height()
       
+      for i in  range(h-1, 0, -1):
+        self.heapify(i)
+        
     def insert(self, item):
       self.heap.append(item)
       current = len(self.heap) -1
       
-      while self.heap[current] < self.heap[self.__parent(current)]:
-        self.__swap(current, self.__parent(current))
-        current = self.__parent(current)
+      while self.heap[current] < self.heap[self.parent_index(current)]:
+        self.__swap(current, self.parent_index(current))
+        current = self.parent_index(current)
         
     def remove(self):
-      min = self.heap.pop(0)
-      self.minHeapify()
+      self.__swap(0, len(self.heap)-1)
+      min = self.heap.pop()
+      self.min_heapify()
       return min
       
     def peek(self):
-      return self.heap.pop()
+      return self.heap[0]
 
     def size(self):
       return len(self.heap)
 
+    # Print the Heap Left and Right Elements
     def print(self):
       for i in range(0, (len(self.heap) // 2)):
-        print ("PARENT :"+  str(self.heap[i]) + "   LEFT CHILD   ", str(self.heap[2*i +1]), "  RIGHT CHILD "+ str(self.heap[2*i + 2]))
-    
-    def heap_height(self):
-      return
+        right_child = 'NO CHILD'
+        if  self.right_child_index(i) < len(self.heap): 
+          right_child = str(self.heap[2*i + 2]) 
+        
+        print ("PARENT :"+  str(self.heap[i]) + "   LEFT CHILD   ", str(self.heap[2*i +1]), "  RIGHT CHILD "+ right_child)
     
 min_heap = MinHeap()
 min_heap.insert(5)
@@ -50,4 +87,6 @@ min_heap.insert(17)
 min_heap.insert(1)
 min_heap.insert(84)
 
+min_heap.print()
+min_heap.remove()
 min_heap.print()
